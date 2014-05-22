f1df823b90ca1eabf3fab41f34f5cf8fea70092e
Marco <marco1@live.de>
2014-05-22 09:21:29 +0200

    Project setup

---
+++ font-awesome-to-png.bat
@@ -0,0 +1,103 @@
+python.exe font-awesome-to-png.py --color #000000 --font fontawesome-webfont.ttf --size 16 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\black\16\"
+python.exe font-awesome-to-png.py --color #000000 --font fontawesome-webfont.ttf --size 32 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\black\32\"
+python.exe font-awesome-to-png.py --color #000000 --font fontawesome-webfont.ttf --size 64 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\black\64\"
+python.exe font-awesome-to-png.py --color #000000 --font fontawesome-webfont.ttf --size 96 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\black\96\"
+python.exe font-awesome-to-png.py --color #000000 --font fontawesome-webfont.ttf --size 128 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\black\128\"
+python.exe font-awesome-to-png.py --color #000000 --font fontawesome-webfont.ttf --size 192 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\black\192\"
+python.exe font-awesome-to-png.py --color #000000 --font fontawesome-webfont.ttf --size 256 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\black\256\"
+python.exe font-awesome-to-png.py --color #000000 --font fontawesome-webfont.ttf --size 512 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\black\512\"
+
+python.exe font-awesome-to-png.py --color #ffffff --font fontawesome-webfont.ttf --size 16 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\white\16\"
+python.exe font-awesome-to-png.py --color #ffffff --font fontawesome-webfont.ttf --size 32 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\white\32\"
+python.exe font-awesome-to-png.py --color #ffffff --font fontawesome-webfont.ttf --size 64 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\white\64\"
+python.exe font-awesome-to-png.py --color #ffffff --font fontawesome-webfont.ttf --size 96 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\white\96\"
+python.exe font-awesome-to-png.py --color #ffffff --font fontawesome-webfont.ttf --size 128 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\white\128\"
+python.exe font-awesome-to-png.py --color #ffffff --font fontawesome-webfont.ttf --size 192 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\white\192\"
+python.exe font-awesome-to-png.py --color #ffffff --font fontawesome-webfont.ttf --size 256 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\white\256\"
+python.exe font-awesome-to-png.py --color #ffffff --font fontawesome-webfont.ttf --size 512 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\white\512\"
+
+python.exe font-awesome-to-png.py --color #0000ff --font fontawesome-webfont.ttf --size 16 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\blue\16\"
+python.exe font-awesome-to-png.py --color #0000ff --font fontawesome-webfont.ttf --size 32 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\blue\32\"
+python.exe font-awesome-to-png.py --color #0000ff --font fontawesome-webfont.ttf --size 64 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\blue\64\"
+python.exe font-awesome-to-png.py --color #0000ff --font fontawesome-webfont.ttf --size 96 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\blue\96\"
+python.exe font-awesome-to-png.py --color #0000ff --font fontawesome-webfont.ttf --size 128 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\blue\128\"
+python.exe font-awesome-to-png.py --color #0000ff --font fontawesome-webfont.ttf --size 192 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\blue\192\"
+python.exe font-awesome-to-png.py --color #0000ff --font fontawesome-webfont.ttf --size 256 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\blue\256\"
+python.exe font-awesome-to-png.py --color #0000ff --font fontawesome-webfont.ttf --size 512 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\blue\512\"
+
+python.exe font-awesome-to-png.py --color #ff0000 --font fontawesome-webfont.ttf --size 16 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\red\16\"
+python.exe font-awesome-to-png.py --color #ff0000 --font fontawesome-webfont.ttf --size 32 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\red\32\"
+python.exe font-awesome-to-png.py --color #ff0000 --font fontawesome-webfont.ttf --size 64 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\red\64\"
+python.exe font-awesome-to-png.py --color #ff0000 --font fontawesome-webfont.ttf --size 96 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\red\96\"
+python.exe font-awesome-to-png.py --color #ff0000 --font fontawesome-webfont.ttf --size 128 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\red\128\"
+python.exe font-awesome-to-png.py --color #ff0000 --font fontawesome-webfont.ttf --size 192 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\red\192\"
+python.exe font-awesome-to-png.py --color #ff0000 --font fontawesome-webfont.ttf --size 256 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\red\256\"
+python.exe font-awesome-to-png.py --color #ff0000 --font fontawesome-webfont.ttf --size 512 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\red\512\"
+
+python.exe font-awesome-to-png.py --color #ffff00 --font fontawesome-webfont.ttf --size 16 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\yellow\16\"
+python.exe font-awesome-to-png.py --color #ffff00 --font fontawesome-webfont.ttf --size 32 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\yellow\32\"
+python.exe font-awesome-to-png.py --color #ffff00 --font fontawesome-webfont.ttf --size 64 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\yellow\64\"
+python.exe font-awesome-to-png.py --color #ffff00 --font fontawesome-webfont.ttf --size 96 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\yellow\96\"
+python.exe font-awesome-to-png.py --color #ffff00 --font fontawesome-webfont.ttf --size 128 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\yellow\128\"
+python.exe font-awesome-to-png.py --color #ffff00 --font fontawesome-webfont.ttf --size 192 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\yellow\192\"
+python.exe font-awesome-to-png.py --color #ffff00 --font fontawesome-webfont.ttf --size 256 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\yellow\256\"
+python.exe font-awesome-to-png.py --color #ffff00 --font fontawesome-webfont.ttf --size 512 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\yellow\512\"
+
+python.exe font-awesome-to-png.py --color #00cc00 --font fontawesome-webfont.ttf --size 16 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\green\16\"
+python.exe font-awesome-to-png.py --color #00cc00 --font fontawesome-webfont.ttf --size 32 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\green\32\"
+python.exe font-awesome-to-png.py --color #00cc00 --font fontawesome-webfont.ttf --size 64 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\green\64\"
+python.exe font-awesome-to-png.py --color #00cc00 --font fontawesome-webfont.ttf --size 96 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\green\96\"
+python.exe font-awesome-to-png.py --color #00cc00 --font fontawesome-webfont.ttf --size 128 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\green\128\"
+python.exe font-awesome-to-png.py --color #00cc00 --font fontawesome-webfont.ttf --size 192 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\green\192\"
+python.exe font-awesome-to-png.py --color #00cc00 --font fontawesome-webfont.ttf --size 256 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\green\256\"
+python.exe font-awesome-to-png.py --color #00cc00 --font fontawesome-webfont.ttf --size 512 ALL
+for %%f in (%~dp0*.png) do move %%f "%~dp0\Icons\green\512\"
+
+pause
